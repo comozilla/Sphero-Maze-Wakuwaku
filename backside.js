a8a5701a1eb3f073b393a8fbe67009908afdbffa
shundroid <ra24483@qb3.so-net.ne.jp>
2016-03-20 20:08:19 +0900

    gifter-labo ブランチを最新化

--- backside.js
+++ backside.js
@@ -1,7 +1,14 @@
 // バックで環境設定などのややこしい処理をします。
 var sphero = require("sphero");
+var keypress = require("keypress");
+
 var events = {};
-module.exports = {
+var moveLoopId = -1;
+var collisionCount = 0;
+var orb;
+var keypressCallbacks = {};
+
+var backside = {
   addEventListener: function(eventName, fn) {
     if (typeof events[eventName] === "undefined") {
       events[eventName] = [];
@@ -9,27 +16,30 @@ module.exports = {
     events[eventName].push(fn);
   },
   connect: function(port, callback) {
-    var orb = sphero(port);
+    orb = sphero(port);
     orb.connect(function() {
       console.log("準備開始");
       orb.startCalibration(); // 位置関係の補正
-      setTimeout(function() {
+      setKeypressCallback("space", function() {
         console.log("準備終了");
         orb.finishCalibration();
         orb.detectCollisions(); // 衝突判定を有効化
         callback(orb);
-      }, 10000);
-    });
-    orb.on("collision", function() {
-      raiseEvent("collision");
+        orb.on("collision", function() {
+          backside.color("green", 0.5);
+          raiseEvent("collision", collisionCount++);
+          // collisionCountは0始まりだけど、↑でインクリメントしてるからそのまま。
+          console.log((collisionCount) + "回目の衝突です");
+        });
+      });
     });
     return orb;
   },
-  move: function(speed, deg, orb) {
+  move: function(speed, deg) {
+    var _deg = 0;
     if (typeof deg === "number") {
-      orb.roll(speed, deg);
+      _deg = deg;
     } else if (typeof deg === "string") {
-      var _deg = 0;
       switch (deg) {
         case "左":
           _deg = 270;
@@ -44,28 +54,73 @@ module.exports = {
           _deg = 0;
           break;
       }
-      orb.roll(speed, _deg);
     }
+    roll(orb, speed, _deg);
   },
-  color: function(orb, color, time) {
-    var _c;
+  color: function(color, time) {
     orb.getColor(function(err, data) {
-      // なぜかdata.colorは、16進数だが文字列として帰ってくるので、parseInt。
-      _c = parseInt(data.color);
-      orb.color(color);
-      if (typeof time !== "undefined") {
-        setTimeout(function() {
-          orb.color(_c);
-        }, time * 1000);
+      if (data) {
+        // なぜかdata.colorは、16進数だが文字列として帰ってくるので、parseInt。
+        var _c = parseInt(data.color);
+        orb.color(color);
+        if (typeof time !== "undefined") {
+          setTimeout(function() {
+            orb.color(_c);
+          }, time * 1000);
+        }
       }
     });
   }
 };
 
 function raiseEvent(eventName) {
+  var args = [];
+  for (var i = 0; i < arguments.length; i++) {
+    if (i === 0) continue;
+    args.push(arguments[i]);
+  }
   if (typeof events[eventName] !== "undefined") {
     events[eventName].forEach(i => {
-      i();
+      i.apply(this, args);
     });
   }
 }
+
+// 継続的に実行するためにラップする
+function roll(orb, speed, degree) {
+  clearTimeout(moveLoopId);
+  orb.roll(speed, degree);
+  moveLoopId = setTimeout(roll, 1000, orb, speed, degree);
+}
+
+function configureKeypress() {
+  // make `process.stdin` begin emitting "keypress" events
+  keypress(process.stdin);
+
+  // listen for the "keypress" event
+  process.stdin.on('keypress', function(ch, key) {
+    if (key && key.ctrl && key.name === 'c') {
+      console.log("exit");
+      process.stdin.pause();
+      process.exit();
+    }
+    if (typeof keypressCallbacks[key.name] !== "undefined") {
+      keypressCallbacks[key.name].forEach(i => {
+        i();
+      });
+    }
+  });
+
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+}
+
+function setKeypressCallback(key, callback) {
+  if (typeof keypressCallbacks[key] === "undefined") {
+    keypressCallbacks[key] = [];
+  }
+  keypressCallbacks[key].push(callback);
+}
+configureKeypress();
+
+module.exports = backside;
--- main.js
+++ main.js
@@ -2,34 +2,20 @@ var backside = require("./backside");
 
 // 自分の Sphero の ID に置き換える
 var port = "COM3";
-var orbDefaultColor = "orange";
-var loopInterval = 1000;
-var orb = {};
 
 // 接続された時に呼び出されます。
 function connect() {
-  backside.color(orb, orbDefaultColor);
-  setTimeout(loop, loopInterval);
+  backside.color("orange");
   // ここに処理を書きます
-  backside.move(50, "前", orb);
+  backside.move(100, "前");
   // ここまで
-}
-
-// メインとなるループ処理（上のloopIntervalの間隔で呼び出されます）
-function loop() {
-  // ここに処理を書きます
-
-  // ここまで
-  setTimeout(loop, loopInterval);
 }
 
 // 衝突時に呼び出されます。
-function collision() {
+function collision(count) {
   // ここに処理を書きます
-  backside.color(orb, "green", 1);
   // ここまで
 }
 
-orb = backside.connect(port, connect);
+backside.connect(port, connect);
 backside.addEventListener("collision", collision);
-backside.addEventListener("loop", loop);
