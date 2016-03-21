8cd1c60b00595beb9776356f808654279b531860
shundroid <ra24483@qb3.so-net.ne.jp>
2016-03-21 11:54:58 +0900

    ziguzagu.js ミスを修正

--- ziguzagu.js
+++ ziguzagu.js
@@ -1,7 +1,7 @@
 // samples/sample-array.js に移行したので消したい
 
-var backside = require("./backside");
-var controller = require("sphero-controller");
+var controller = require("./controller");
+var keypress = require("keypress");
 
 // 自分の Sphero の ID に置き換える
 var port = "COM7";
@@ -13,26 +13,25 @@ var currentAnglePoint = 0;
 var graph = [];
 
 // 接続された時に呼び出されます。
-function connect() {
-  backside.color("orange");
+function onConnected() {
+  controller.setColor("orange");
   setTimeout(loop, loopInterval);
   // ここに処理を書きます
-  backside.move(255, angles[Math.min(currentAnglePoint++, angles.length - 1)]);
+  controller.move(255, angles[Math.min(currentAnglePoint++, angles.length - 1)]);
   // ここまで
 }
 
 // 衝突時に呼び出されます。
-function collision(index) {
+function onCollised(index) {
   // ここに処理を書きます
   console.log(index);
-  backside.color("red");
+  controller.setColor("red");
   setTimeout(function() {
-    backside.color("green");
+    controller.setColor("green");
   }, 500);
-  backside.move(255, angles[Math.min(currentAnglePoint++, angles.length - 1)]);
+  controller.move(255, angles[Math.min(currentAnglePoint++, angles.length - 1)]);
   // ここまで
 }
 
-backside.connect(port, connect);
-backside.addEventListener("collision", collision);
-backside.addEventListener("loop", loop);
+controller.connect(port, onConnected);
+controller.addEventListener("collision", onCollised);
